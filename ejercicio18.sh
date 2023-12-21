b5b3e22559a5fc757176e87fbc67d7a21bc81d4c
alufer8673 <alufer8673@ieselcaminas.org>
2023-12-21 09:54:28 +0100

    2023_12_21

---
+++ ejercicio18.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -eq 1 ]]; then
+    
+else
+    echo "ERROR: Debes introducir un sólo parámetro"
+    echo "La forma correcta de ejecutar el script es:"
+    echo "  ./ejercicio18.sh [usuario]"
+fi
